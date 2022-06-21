b1e7b34454a96accb55a11c3f99cc49081b924eb
hvphan <hvphan1993@gmail.com>
2022-06-21 11:38:11 -0700

    cleaned up dashboard and templates

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const sequelize = require('./config/connection');
 const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
+
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
@@ -16,8 +20,6 @@ const sess = {
     db: sequelize
   })
 };
-const helpers = require('./utils/helpers');
-const hbs = exphbs.create({ helpers });
 
 app.use(session(sess));
 
@@ -28,7 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
