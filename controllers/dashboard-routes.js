c8dabe9b9e1b91de8779925054fa4c81561f09de
hvphan <hvphan1993@gmail.com>
2022-06-22 11:10:50 -0700

    completed dashboard-route to allow edit/delete page to appear

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -36,7 +36,7 @@ router.get('/', withAuth, (req, res) => {
     .then(dbPostData => {
         // serialize data before passing to template
         const posts = dbPostData.map(post => post.get({ plain: true }));
-        res.render('dashboard', { loggedIn: true });
+        res.render('dashboard', { posts, loggedIn: true });
     })
     .catch(err => {
         console.log(err);
